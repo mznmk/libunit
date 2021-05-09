a91d615dbebb3588991d8a396f8965f2d96bd307
mznmk <mika_develop@mznmk.com>
2021-05-10 01:23:51 +0900

    bugfix real_tests/include/test_strlen (little bit bug)

--- real_tests/include/test_strlen.h
+++ real_tests/include/test_strlen.h
@@ -6,7 +6,7 @@
 /*   By: mmizuno <mmizuno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 00:07:31 by mmizuno           #+#    #+#             */
-/*   Updated: 2021/05/10 01:04:32 by mmizuno          ###   ########.fr       */
+/*   Updated: 2021/05/10 01:23:18 by mmizuno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@
 # include <string.h>
 
 # include "../../framework/include/libunit.h"
-# include "tested_functions.h"
 
 /*
 ** =========================== prototype declaration ===========================
